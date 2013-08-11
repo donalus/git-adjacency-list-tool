671f27024995173615ef573a2ca74bddf6cae96a
donalus <donal.h@gmail.com>
2013-08-11 11:46:23 -0400

    Forgot to add changes to README.md
    Adding some test code before deleting for posterity of the failed attempt

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ git-adjacency-list-tool
 =======================
 
 The simple tool for creating an adjacency list from a git repository for use in common social network analysis tools
+
+This is rough at this point and more of a test. Once the functionality is proven, then it will be cleaned up considerably.
+
+This project was created as part of the final paper for INST633 Analyzing Social Networks and Social Media at the University of Maryland.
--- galt.py
+++ galt.py
@@ -1,23 +1,47 @@
+#! /usr/bin/python
 from pygit2 import Repository
-from pygit2 import GIT_SORT_TIME, GIT_SORT_REVERSE
 from pygit2 import Tree
+from pygit2 import GIT_SORT_TIME, GIT_SORT_REVERSE
 
-repo = Repository('/home/donal/git')
+def find_changes(commit):
+    if len(commit.parents) > 0:
+        for parent in commit.parents:
+            for entry in commit.tree:
+                if entry.name in parent.tree:
+                    prev_oid = parent.tree[entry.name].oid
+                    oid = commit.tree[entry.name].oid
+                    has_changed = (oid != prev_oid)
+                    if has_changed:
+                        yield commit.author.email, entry.name, entry.filemode
+                else:
+                    yield commit.author.email, entry.name, entry.filemode
+    else:
+        for entry in commit.tree:
+            yield commit.author.email, entry.name, entry.filemode
 
-def iter_log():
-	for commit in repo.walk(repo.head.target, GIT_SORT_TIME):
-		if len(commit.parents) > 0:
-			parent = commit.parents[0]
-		for entry in commit.tree:
-			if entry.name in parent.tree:
-				prev_oid = parent.tree[entry.name].oid
-				oid = commit.tree[entry.name].oid
-				has_changed = (oid != prev_oid)
-				if has_changed:
-					yield commit.author.email, entry.name, entry.filemode
-			
-			
+def find_changes2(commit):
+    ct = commit.tree
+    if len(commit.parents) > 0:
+        for parent in commit.parents:
+            pt = parent.tree
+            diff = pt.diff_to_tree(ct)
+            for patch in diff:
+                yield commit.author.email, patch.new_file_path
+    else:
+        diff = ct.diff_to_tree()
+        for patch in diff:
+            yield commit.author.email, patch.new_file_path
 
-changes = iter_log()
-for c in changes:
-	print c
+def walk_commits(repo):
+    for commit in repo.walk(repo.head.target, GIT_SORT_TIME):
+        changes = find_changes(commit)
+        for c in changes:
+            print c
+    return changes
+
+def main():
+    repo = Repository('/home/donal/gittest')
+    changes = walk_commits(repo)
+
+if __name__ == "__main__":
+    main()
